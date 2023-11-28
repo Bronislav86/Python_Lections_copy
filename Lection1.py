20e56aee6ff40cad5c66c175c884a148a9ed0199
Bronislav <shevchenko-bg@yandex.ru>
2023-11-28 21:04:28 +0300

    Init commit

---
+++ Lection1.py
@@ -0,0 +1,75 @@
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# # print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+# print("Введите первое число")
+# a = input()
+
+# b = input('Введите второе число:')
+
+# print(a, ' + ', b, ' = ', a + b )
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c + '89')
+# print(type(c))
+
+# v = 'qwerty'
+# v = int(v)
+
+# print("Введите первое число")
+# a = int(input())
+# b = int(input('Введите второе число:'))
+
+# print(a, ' + ', b, ' = ', a + b )
+
+# a = 5.89956
+# b = 6.556551
+# print(round(a*b, 3))
+
+#                                                   ОТСТУПЫ
+
+# if conditions:
+
+#     operator 1
+
+#     operator 2
+
+#     ...
+
+#     operator n
+
+# else:
+    
+#     operator n + 1
+
+#     operator n + 2
+
+#     ...
+
+#     operator n + m
+
+# username = input('Ведите имя: ')
+# if username == 'Маша':
+#     print('Ураб это же МАША')
+# elif username == 'Марина':
+#     print('Я так ждал Васб Марина')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ')
+# else:
+#     print('Привет, ', username)
+
+# a = 'qwerty'
+
+#print(a[0])
+# for i in a:
+#     print(i)
+
